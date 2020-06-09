727445005c61dea249546462e2a3fc36eda2dcd6
tranminhchien1406 <PC@NOZOMU>
2020-06-09 12:02:18 +0700

    Chỉnh sửa trang View Khóa học: Views/Course/Create.cshtml

--- TranMinhChien_Web_lab456/ViewModels/CourseViewModel.cs
+++ TranMinhChien_Web_lab456/ViewModels/CourseViewModel.cs
@@ -2,13 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using TranMinhChien_Web_lab456.Models;
 
 namespace TranMinhChien_Web_lab456.ViewModels
 {
     public class CourseViewModel
     {
-        public string Place { get; set;}
+        public string Place { get; set; }
         public string Date { get; set; }
         public string Time { get; set; }
+        public byte Category { get; set; }
+        public IEnumerable<Category> Categories { get; set; }
     }
 }
