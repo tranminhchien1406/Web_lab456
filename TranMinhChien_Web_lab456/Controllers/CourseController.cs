479c04d303dd13f79db7c033e72631425731174b
tranminhchien1406 <PC@NOZOMU>
2020-06-10 20:07:48 +0700

    File Controllers/CoursesController.cs (Kiểm tra dữ liệu nhập phía Server, trước khi lưu vào cơ sở dữ liệu, nếu nhập sai yêu cầu lỗi sẽ được gửi lại trang đang thao tác)  Trang 22

--- TranMinhChien_Web_lab456/Controllers/CourseController.cs
+++ TranMinhChien_Web_lab456/Controllers/CourseController.cs
@@ -3,15 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TranMinhChien_Web_lab456.Migrations;
+using TranMinhChien_Web_lab456.Models;
+using TranMinhChien_Web_lab456.ViewModels;
 
 namespace TranMinhChien_Web_lab456.Controllers
 {
     public class CourseController : Controller
     {
         // GET: Course
-        public ActionResult Create()
+        private readonly ApplicationDbContext _dbContext;
+        public CourseController()
         {
-            return View();
+            _dbContext = new ApplicationDbContext();
+        }
+        [Authorize]
+        public ActionResult Create(CourseViewModel viewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                viewModel.Categories = _dbContext.Categories.ToList();
+                return View("Create", viewModel);
+            }    
+            var course = new Course
+            {
+                LecturerId = ApplicationUser.Indentity.GetUserId(),
+                DateTime = viewModel.GetDateTime(),
+                CategogryId = viewModel.Category,
+                Place = viewModel.Place
+            };
+            _dbContext.Course.Add(course);
+            _dbContext.SaveChanges();
+            return View(viewModel);
         }
     }
 }
--- TranMinhChien_Web_lab456/ViewModels/CourseViewModel.cs
+++ TranMinhChien_Web_lab456/ViewModels/CourseViewModel.cs
@@ -8,9 +8,13 @@ namespace TranMinhChien_Web_lab456.ViewModels
 {
     public class CourseViewModel
     {
+        [Required]
         public string Place { get; set; }
+        [Required]
         public string Date { get; set; }
+        [Required]
         public string Time { get; set; }
+        [Required]
         public byte Category { get; set; }
         public IEnumerable<Category> Categories { get; set; }
     }
