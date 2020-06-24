59013186c448b25d040d7266d35bbf907654e5ba
tranminhchien1406 <PC@NOZOMU>
2020-06-24 19:08:07 +0700

    6.11 Views/Courses/Mine.cshtml

--- TranMinhChien_Web_lab456/Controllers/CourseController.cs
+++ TranMinhChien_Web_lab456/Controllers/CourseController.cs
@@ -75,6 +75,22 @@ namespace TranMinhChien_Web_lab456.Controllers
                 .Tolist();
             return View(courses);
         }
-    
+        
+        [Authorize]
+        public ActionResult Edit(int id) 
+        {
+            var userId = User.Identity.GetUserId();
+            var course = _dbContext.Courses.Single(c => c.Id == id && c.LecturerId == userId);
+            var viewModel = new CourseViewModel
+            {
+                Categories = _dbContext.Categories.ToList(),
+                Date = course.DateTime.ToString("dd/M/yyyy"),
+                Time = course.DateTime.ToString("HH:mm"),
+                Category = course.CategoryId,
+                Place = course.Place
+            };
+            return View("Create", viewModel);
         }
+
+    }
 }
